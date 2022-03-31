0db6f971f2e42c28c72437bb31067ad93b85361f
kyo_s <kyosyunn@yahoo.co.jp>
2022-03-31 18:26:18 +0900

    配列がからのケースを対応

--- solution2_1/solution2_1.go
+++ solution2_1/solution2_1.go
@@ -1,13 +1,16 @@
 package solution21
 
-import "fmt"
-
 func Solution(A []int, K int) []int {
 
 	// Aに入ってる配列をKの個数だけ動かす。
 
 	// 同じ長さの配列の作成
 	res := make([]int, len(A))
+
+	// 配列の長さが0件だった場合は即座に終了
+	if len(A) == 0 {
+		return A
+	}
 
 	// 回転数の確認
 	rotate := K % len(A)
@@ -17,8 +20,6 @@ func Solution(A []int, K int) []int {
 		if newPosi > len(A)-1 {
 			newPosi -= len(A)
 		}
-		fmt.Printf("i: %d, rotate: %d, newPosi: %d\n", i, rotate, newPosi)
-		fmt.Printf("★res[%d] <= %d \n", newPosi, A[i])
 		res[newPosi] = A[i]
 	}
 	return res
